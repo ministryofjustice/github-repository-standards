910cd6006dfae266c5e9ce22abbeb622770d1b58
Nick Walters <nick.walters@digital.justice.gov.uk>
2022-01-24 17:17:01 +0000

    add print and sleep to GH API call

--- lib/issue_creator.rb
+++ lib/issue_creator.rb
@@ -12,8 +12,10 @@ class GithubRepositoryStandards
 
     def create_issue
       if issue_already_exists.empty?
+        puts "Create issue in repository: #{repository}"
         url = "https://api.github.com/repos/#{owner}/#{repository}/issues"
         HttpClient.new.post_json(url, issue_hash.to_json)
+        sleep 5
       end
     end
 
